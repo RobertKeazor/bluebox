8b2edab19a237bbfcdeb1e663eba1b1e61f31284
Anyer Castillo <anyer.castillo@gmail.com>
2015-06-26 00:58:56 -0400

    Added Roboguice as a DI

--- app/src/main/java/com/mac/bluebox/MainActivity.java
+++ app/src/main/java/com/mac/bluebox/MainActivity.java
@@ -7,34 +7,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }*/
+@ContentView(R.layout.activity_main)
+public class MainActivity extends RoboActivity {
+    @InjectView(R.id.textViewHello)
+    TextView textViewHello;
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onResume() {
+        super.onResume();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        textViewHello.setText("Bluebox Rocks !!!!");
     }
 }
---
+++ app/src/main/java/com/mac/bluebox/roboguice/AppModule.java
@@ -0,0 +1,17 @@
+package com.mac.bluebox.roboguice;
+
+import android.app.Application;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * Created by anyer on 6/26/15.
+ */
+public class AppModule extends AbstractModule {
+    public AppModule(Application application) {
+    }
+
+    @Override
+    protected void configure() {
+    }
+}
