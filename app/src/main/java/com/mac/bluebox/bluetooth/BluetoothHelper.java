78d37c509d99ff5e5417f37ff8580924958ebe3d
arturo <arturo3883@gmail.com>
2015-06-26 17:07:21 -0400

    conexion a dispositivo por servicio

--- app/src/main/java/com/mac/bluebox/bluetooth/BluetoothHelper.java
+++ app/src/main/java/com/mac/bluebox/bluetooth/BluetoothHelper.java
@@ -1,11 +1,24 @@
 package com.mac.bluebox.bluetooth;
 
+import android.app.IntentService;
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.IBinder;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by anyer on 6/26/15.
  */
-public class BluetoothHelper {
+public class BluetoothHelper extends Service {
+
+    private BluetoothDevice bluetoothDevice;
+
     public List<String> getOnlineDevices() {
         return null;
     }
@@ -13,4 +26,40 @@ public class BluetoothHelper {
     public void connect(String friend) {
 
     }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        BluetoothDevice bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+        connectToDevice(bluetoothDevice);
+        return null;
+    }
+
+    private void connectToDevice(BluetoothDevice bluetoothDevice) {
+        BluetoothSocket bluetoothSocket = null;
+        UUID blueBoxUuid = new UUID(1000001, 100000001);
+        try {
+            bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(blueBoxUuid);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            // Connect the device through the socket. This will block
+            // until it succeeds or throws an exception
+            bluetoothSocket.connect();
+        } catch (IOException connectException) {
+            // Unable to connect; close the socket and get out
+            try {
+                bluetoothSocket.close();
+            } catch (IOException closeException) { }
+            return;
+        }
+
+        // Do work to manage the connection (in a separate thread)
+        handleDeviceConnected(bluetoothSocket);
+    }
+
+    private void handleDeviceConnected(BluetoothSocket bluetoothSocket) {
+        //do stuff
+    }
+
 }
